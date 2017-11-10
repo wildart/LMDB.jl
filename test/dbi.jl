bf9394803ad76c874211a5dfc2a6f7f86ae67650
tan <tanmaykm@gmail.com>
2017-11-11 00:01:56 +0530

    allow delete without comparing value
    This will allow delete of all values for a key when no `val` is supplied to the `delete!` method.
    This is done by passing a `C_NULL` as value (as in the C API doc).

--- test/dbi.jl
+++ test/dbi.jl
@@ -38,6 +38,7 @@ module LMDB_DBI
                     value = get(txn, dbi, k, AbstractString)
                     println("Got value for key $(k): $(value)")
                     @test value == val*string(k)
+                    delete!(txn, dbi, k)
                     k += 1
                     value = get(txn, dbi, k, AbstractString)
                     println("Got value for key $(k): $(value)")
